3d09b952e4587818263ae535275930699415e844
Thanuja <thanujatha1432@gmail.com>
2026-07-03 11:55:19 +0530

    Added Module1 exercises

---
+++ Deep skilling/Module1/Exercise1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Logger
+{
+    private static Logger instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger object created");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+            instance = new Logger();
+
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("Log: " + message);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Logger obj1 = Logger.GetInstance();
+        obj1.Log("First message");
+
+        Logger obj2 = Logger.GetInstance();
+        obj2.Log("Second message");
+
+        if (obj1 == obj2)
+            Console.WriteLine("Both are same object");
+    }
+}
---
+++ Deep skilling/Module1/Exercise2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+interface IDocument
+{
+    void Open();
+}
+
+class WordDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Word Document Opened");
+    }
+}
+
+class PdfDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("PDF Document Opened");
+    }
+}
+
+class ExcelDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Excel Document Opened");
+    }
+}
+
+class DocumentFactory
+{
+    public static IDocument CreateDocument(string type)
+    {
+        if (type == "word")
+            return new WordDocument();
+        else if (type == "pdf")
+            return new PdfDocument();
+        else if (type == "excel")
+            return new ExcelDocument();
+        else
+            return null;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IDocument doc1 = DocumentFactory.CreateDocument("word");
+        doc1.Open();
+
+        IDocument doc2 = DocumentFactory.CreateDocument("pdf");
+        doc2.Open();
+
+        IDocument doc3 = DocumentFactory.CreateDocument("excel");
+        doc3.Open();
+    }
+}
